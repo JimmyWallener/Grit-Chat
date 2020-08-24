4452c2be90778f4c6e7b6a0a06db215a60a9c98d
Jimmy Wallener <38855083+jimwdev@users.noreply.github.com>
2020-08-24 14:23:06 +0200

    update 2

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const userId = location.hash.slice(1);
+console.log(userId);
+
+let peer = new Peer(userId, {
+    host: 'glajan.com',
+    port: 8443,
+    path: '/myapp',
+    secure: true
+});
+
+
+peer.on('open', peerOnConnection = (id) => {
+    console.log('My peer id is: ' + id);
+    document.querySelector('.my-peer-id').innerHTML = id;
+});
+
+
 
