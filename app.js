752aab32af2cb2eacb57f4e343c91e201a231953
Jimmy Wallener <38855083+jimwdev@users.noreply.github.com>
2020-08-24 15:31:43 +0200

    added popup

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+let userHandle = prompt("Enter your name:",);
+let url = window.location.href;
+function changeUrl(url, userHandle) {
+    var new_url = url + '#' + userHandle;
+    window.history.pushState('data', 'Title', new_url);
+    document.title = "Grit-Chat: Logged in as " + userHandle;
+}
+changeUrl(url, userHandle);
+console.log(userHandle);
 const userId = location.hash.slice(1);
+
 console.log(userId);
 
 let peer = new Peer(userId, {
@@ -15,4 +25,12 @@ peer.on('open', peerOnConnection = (id) => {
 });
 
 
+let isEven = num => console.log(num % 2 == 0 ? 'Is Even' : 'Is Odd');
 
+function isOdd(num) {
+    let test = num % 2 == 0 ? 'Is Even' : 'Is Odd';
+    console.log(test);
+}
+
+isEven(2);
+isOdd(3);
